a2cfc5db20f1391fdeec70507a1ca45624df6371
Peter A. VAN LOON <captvanloonpa@gmail.com>
2019-04-14 11:07:00 -0400

    Editing to set up Restaurant Functionality

--- database_setup.py
+++ database_setup.py
@@ -1,26 +1,27 @@
 import sys
+import os
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.net.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base
 
 class Restaurant(Base):
-	__tablename__ = 'restaurant'
+    __tablename__ = 'restaurant'
 
-	name = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
 
 class MenuItem(Base):
-	__tablename__ = 'menu_item'
-	name = Column(String(80), nullable = False)
-	id = Column(Integer, primary_key = True)
-	course = Column(String(250))
-	price = Column(String(8))
-	description = Column(String(250))
-	restaurant_id = Column(Integer, ForeignKey('restaurant_id'))
-	restaurant = relationship(Restaurant)
+    __tablename__ = 'menu_item'
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    course = Column(String(250))
+    price = Column(String(8))
+    description = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant_id'))
+    restaurant = relationship(Restaurant)
 
 #We added this serialize function to be able to send JSON objects in a serializable format
     @property
